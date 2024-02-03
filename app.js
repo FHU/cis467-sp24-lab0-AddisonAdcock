ebfc241d0d92fafc52ce045afcb2c8ca289bf9a6
AddisonAdcock <addison.adcock@students.fhu.edu>
2024-02-02 18:40:37 -0600

    Greet

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.get('/greet', (req, res)=> {
     console.log(req.query)
 
     res.send(`hey, ${req.query.name}`)
+    res.send(`You are  ${2024 - req.query.dob} or ${2023 - req.query.dob} years old`)
 })
 
 app.get('/math/:num1/:op/:num2', (req, res)=> {
